8503c6fd0e77188a1949b41e7012da26f2c632e6
Chantheman23 <74678113+chandlerowen23@users.noreply.github.com>
2021-02-09 19:39:13 -0700

    Update board.py

--- mastermind_template/mastermind/game/board.py
+++ mastermind_template/mastermind/game/board.py
@@ -1,44 +1,63 @@
 import random
 
-
-
-class Board():
-    """creates board with random number of piles and stones
+class Board:
+    """A designated playing surface. The responsibility of Board is to keep track of the pieces in play.
     
+    Stereotype: 
+        Information Holder
+
     Attributes:
-        _piles is list of piles and data contained in them.
-        _prepare is a private function to call the prepare function and set stones per pile
+        _piles (list): The number of piles of stones.
     """
     def __init__(self):
-        # Private Attribute
-        self._piles = []
+        """The class constructor.
+        
+        Args:
+            self (Board): an instance of Board.
+        """
+        self.num1 = 0
+        self.num2 = 0
         self._prepare()
 
-    # Private function
-    def _prepare(self):
-        piles = random.randint(2,5)
-        for i in range(piles):
-            stones = random.randint(1,9)
-            self._piles.append(stones)
-
-    # Public Function
     def apply(self, move):
-        pile = move.get_pile()
-        stones = move.get_stones()
-        self._piles[pile] = max(0, self._piles[pile] - stones)
+        """Applies the given move to the playing surface. In this case, that 
+        means removing a number of stones from a pile.
+        
+        Args:
+            self (Board): an instance of Board.
+            move (Move): The move to apply.
+        """
     
-    # Public Function
     def is_empty(self):
-        empty = [0]*len(self._piles)
-        return self._piles == empty
-    
-    # Public Function
-    def to_string(self):
-        #Text is called from the director to display on the console
-        #the text variable must be added to in order to print all the needed information
-        text = "\n--------------------"
-        for pile, stones in enumerate(self._piles):
-            text += (f"\n{pile}: " + "0" * stones)
-        text += "\n--------------------"
-        return text
+        """Determines if all the stones have been removed from the board.
+        
+        Args:
+            self (Board): an instance of Board.
 
+        Returns:
+            boolean: True if the board has no stones on it; false if otherwise.
+        """
+        pass
+
+    def to_string(self):
+        """Converts the board data to its string representation.
+
+        Args:
+           self (Board): an instance of Board.
+
+        Returns:
+            string: A representation of the current board.
+        """ 
+        pass
+
+    def _prepare(self):
+        """Sets up the board with a random number of piles containing a random 
+        number of stones.
+        
+        Args:
+            self (Board): an instance of Board.
+        """
+        self.num1 = random.randint(1000, 9999)
+        self.num2 = random.randint(1000, 9999)
+
+        
