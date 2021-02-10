635171898e0fb6f9aaf53f149173858dc794f9ab
Chantheman23 <74678113+chandlerowen23@users.noreply.github.com>
2021-02-09 19:39:35 -0700

    Update console.py

--- mastermind_template/mastermind/game/console.py
+++ mastermind_template/mastermind/game/console.py
@@ -1,5 +1,3 @@
-import random
-
 class Console:
     """A code template for a computer console. The responsibility of this 
     class of objects is to get text or numerical input and display text output.
